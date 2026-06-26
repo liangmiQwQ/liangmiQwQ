48a44b732dc7bac79c497c5fd8945cdd64d14c3b
Liang Mi <github@liangmi.dev>
2026-06-26 19:12:51 +0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { cli } from '@liangmi/vp-config'
 import vue from '@vitejs/plugin-vue'
 
-function createPackVuePlugins() {
+function createVuePlugins() {
   return [vue()] as never
 }
 
@@ -12,12 +12,16 @@ const config = cli({
   fmt: {
     ignorePatterns: ['./introduce.svg']
   },
+  test: {
+    environment: './tests/tui-node-environment.ts'
+  },
   pack: {
-    plugins: createPackVuePlugins(),
+    plugins: createVuePlugins(),
     entry: ['./src/index.ts']
   }
 })
 
+config.plugins = createVuePlugins()
 config.lint!.plugins = config.lint?.plugins?.filter(e => e !== 'react')
 
 export default config
